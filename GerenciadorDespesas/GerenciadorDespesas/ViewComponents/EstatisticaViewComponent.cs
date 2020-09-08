f6d4409a570ba0c39481d6e6077a157e265f4a70
Heber Gustavo <heber.gbarbosa@hotmail.com>
2020-09-08 16:48:31 -0300

    Criação da sessão de Estatisticas das Despesas

---
+++ GerenciadorDespesas/GerenciadorDespesas/ViewComponents/EstatisticaViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using GerenciadorDespesas.Models;
+using GerenciadorDespesas.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.ViewComponents
+{
+    public class EstatisticaViewComponent : ViewComponent
+    {
+        private readonly Contexto _contexto;
+
+        public EstatisticaViewComponent(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            EstatisticaViewModel estatistica = new EstatisticaViewModel();
+
+            estatistica.QuantidadeDespesas = _contexto.Despesas.Count();
+            estatistica.MaiorDespesa = _contexto.Despesas.Max(x => x.Valor);
+            estatistica.MenorDespesa = _contexto.Despesas.Min(x => x.Valor);
+
+            return View(estatistica);
+        }
+    }
+}
---
+++ GerenciadorDespesas/GerenciadorDespesas/ViewModels/EstatisticaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.ViewModels
+{
+    public class EstatisticaViewModel
+    {
+        public int QuantidadeDespesas { get; set; }
+
+        public double MenorDespesa { get; set; }
+
+        public double MaiorDespesa { get; set; }
+    }
+}
